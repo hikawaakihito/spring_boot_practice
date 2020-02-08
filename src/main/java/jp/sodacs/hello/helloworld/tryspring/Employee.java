d33368e3f29f41f8e62f65c91f4f69f7a62f4d54
Hikawa Akihito <hikawaakihito@gmail.com>
2020-02-08 17:30:56 +0900

    first commit

---
+++ src/main/java/jp/sodacs/hello/helloworld/tryspring/Employee.java
@@ -0,0 +1,10 @@
+package jp.sodacs.hello.helloworld.tryspring;
+
+import lombok.Data;
+
+@Data
+public class Employee{
+    private int employeeId;
+    private String employeeName;
+    private int age;
+}
---
+++ src/test/java/jp/sodacs/hello/helloworld/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.sodacs.hello.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
